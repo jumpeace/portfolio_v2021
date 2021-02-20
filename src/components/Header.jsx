c9d8281f128799c2612c26ee649e357231d00f9f
Jumpei <jumpei141107@gmail.com>
2021-02-21 00:10:19 +0900

    #1 Portfolio完成(レスポンシブ非対応)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./css/Header.css";
 
 const Header = () => {
     return (
-        <div bg="dark" variant="dark" className="header">
+        <div className="header">
             {/* font-family: "Caveat"; のための読み込み */}
             <link
                 href="https://fonts.googleapis.com/css?family=Caveat"
@@ -11,29 +11,24 @@ const Header = () => {
 
             <div className="header-title">JPig</div>
 
-            <nav xs={9} className="header-nav">
+            <nav className="header-links">
                 <a
                     href="mailto:jpig.main@gmail.com"
-                    className="header-nav-item"
+                    className="header-links-item"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     Contact
                 </a>
-            </nav>
-            <div xs={2} className="header-links">
                 <a
-                    href="https://github.com/JPig-Main/Portfolio"
+                    href="https://github.com/JPig-Main/"
                     className="header-links-item"
+                    target="_blank"
+                    rel="noopener noreferrer"
                 >
-                    <img
-                        src={`${process.env.PUBLIC_URL}/img/sns_icons/github.png`}
-                        alt="github"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    />
+                    GitHub
                 </a>
-            </div>
+            </nav>
         </div>
     );
 };
